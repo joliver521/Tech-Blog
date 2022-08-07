ee15dc4287e42c584b47db104e5ff1cc65c383f0
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-07 17:01:34 -0400

    updated post route login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -69,7 +69,29 @@ router.post('/', (req, res) => {
 });
 
 // login a user
-router.post('/login', (req, res) => {});
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username,
+        },
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that username' });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+            res.json({ user: dbUserData, message: 'You are now logged in' });
+        });
+    });
+});
 
 // logout a user
 router.post('/logout', (req, res) => {});
