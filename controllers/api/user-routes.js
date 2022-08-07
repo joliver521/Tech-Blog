9d90544187a5bfbffd9e0d96d80133910fe3a34b
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-07 14:27:44 -0400

    updated get one user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,7 +14,39 @@ router.get('/', (req, res) => {
 });
 
 // get one user
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id,
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_text', 'created_at'],
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['id', 'title', 'post_text', 'created_at'],
+                },
+            },
+        ],
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // create a new user
 router.post('/', (req, res) => {});
