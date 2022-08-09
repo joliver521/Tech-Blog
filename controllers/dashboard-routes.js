0be066ade53ccbd067311aadbb06b881b6bc824b
jso-work <joliver521@gmail.com>
2022-08-09 17:02:14 -0400

    delete-post.js and edit-post.js updated

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,91 +3,79 @@ const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
-    console.log('======================');
-    Post.findAll({
-        where: {
-            user_id: req.session.user_id,
-        },
-        attributes: ['id', 'post_text', 'title', 'created_at'],
-        include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'comment_text',
-                    'post_id',
-                    'user_id',
-                    'created_at',
-                ],
-                include: {
-                    model: User,
-                    attributes: ['username'],
-                },
-            },
-            {
-                model: User,
-                attributes: ['username'],
-            },
-        ],
-    })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('dashboard', {
-                posts,
-                loggedIn: true,
-                username: req.session.username,
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+	console.log('======================');
+	Post.findAll({
+		where: {
+			user_id: req.session.user_id,
+		},
+		attributes: ['id', 'post_text', 'title', 'created_at'],
+		include: [
+			{
+				model: Comment,
+				attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+				include: {
+					model: User,
+					attributes: ['username'],
+				},
+			},
+			{
+				model: User,
+				attributes: ['username'],
+			},
+		],
+	})
+		.then((dbPostData) => {
+			const posts = dbPostData.map((post) => post.get({ plain: true }));
+			res.render('dashboard', {
+				posts,
+				loggedIn: true,
+				username: req.session.username,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id,
-        },
-        attributes: ['id', 'post_text', 'title', 'created_at'],
-        include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'comment_text',
-                    'post_id',
-                    'user_id',
-                    'created_at',
-                ],
-                include: {
-                    model: User,
-                    attributes: ['username'],
-                },
-            },
-            {
-                model: User,
-                attributes: ['username'],
-            },
-        ],
-    })
-        .then(dbPostData => {
-            if (dbPostData) {
-                const post = dbPostData.get({ plain: true });
+	Post.findOne({
+		where: {
+			id: req.params.id,
+		},
+		attributes: ['id', 'post_text', 'title', 'created_at'],
+		include: [
+			{
+				model: Comment,
+				attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+				include: {
+					model: User,
+					attributes: ['username'],
+				},
+			},
+			{
+				model: User,
+				attributes: ['username'],
+			},
+		],
+	})
+		.then((dbPostData) => {
+			if (dbPostData) {
+				const post = dbPostData.get({ plain: true });
 
-                res.render('edit-post', {
-                    post,
-                    loggedIn: true,
-                    username: req.session.username,
-                });
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+				res.render('edit-post', {
+					post,
+					loggedIn: true,
+					username: req.session.username,
+				});
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
